4a89e1e7599e1079cf2938e31a89ede9cee6e8e8
G.I <G.I@gmail.com>
2023-02-03 00:43:28 +0100

    fixing button focus with ref hook

--- src/components/AddItem.jsx
+++ src/components/AddItem.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React , {useRef} from "react";
 import { FaPlus } from "react-icons/fa";
 
 function AddItem({newItem,setNewItem , handleSubmit}) {
+  const inputRef = useRef(); 
   return (
     <form className="addForm" onSubmit={handleSubmit}>
       <label htmlFor="addItem">Add Item</label>
       <input
         autoFocus
+        ref={inputRef}
         id="addItem"
         placeholder="Add Item"
         required
@@ -17,6 +19,7 @@ function AddItem({newItem,setNewItem , handleSubmit}) {
       <button 
       type="submit" 
       aria-label="Add Item"
+      onClick={()=> inputRef.current.focus()}
       >
         <FaPlus />
       </button>
